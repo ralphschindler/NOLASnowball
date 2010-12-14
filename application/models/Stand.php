2b5e1b2749e33a3966e7a176c86511eed5d5d136
Ralph Schindler <ralphschindler@yahoo.com>
2010-12-13 21:32:43 -0600

    Altered doctrine entity-manager configuration Added initial entity

---
+++ application/models/Stand.php
@@ -0,0 +1,12 @@
+<?php
+namespace Application\Model;
+
+class Stand
+{
+    protected $id;
+    protected $name;
+    protected $address;
+    protected $city;
+    protected $state;
+    protected $zipCode;
+}
